a88e051a1db1c26e1de1c7f969b127503aa65e62
AdamSzambur <47151141+AdamSzambur@users.noreply.github.com>
2024-06-20 09:10:43 +0200

    dodajemy robota

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@
 public class Main {
     public static void main(String[] arg) {
         System.out.println('Hello world !!!');
+
+        Robot robot = new Robot('r2d2');
+
+        System.out.println(robot.getName());
     }
 }
